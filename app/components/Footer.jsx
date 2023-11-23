8612109f600f672b30aad2359a0189b09edf45f8
Shane Murphy <shn.murphy@gmail.com>
2023-11-23 11:41:45 +0000

    footer logo svg building issue

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import FooterLogo from "/public/Footerlogo.svg";
+import FooterLogo from "/public/FooterLogo.svg";
 import Link from "next/link";
 export default function Footer() {
   return (
